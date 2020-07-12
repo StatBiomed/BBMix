e9c07da11fa376482d66b1740d2ef9fd3a769b7f
Yuanhua Huang <huangyh094@gmail.com>
2020-07-12 22:35:51 +0800

    add binomial mixture VB implementation

---
+++ bbmix/models/binomial_mix_vb.py
@@ -0,0 +1,221 @@
+# Variational inference of binomial mixture model
+"""
+Variational Inference for binomial mixture model
+Initial development: Yuanhua Huang
+"""
+
+import numpy as np
+from scipy.stats import entropy
+from scipy.sparse import isspmatrix
+from scipy.optimize import minimize
+from scipy.special import logsumexp, digamma
+from .statistics import dirichlet_entropy, logbincoeff, normalize, normalize_exp
+
+class BinomialMixVB():
+    """BinMixVB model: Variational Inference for binomial mixture model
+    The prior can be set via set_prior() before fitting the model.
+    """
+    def __init__(self, n_obs, n_var=1, n_components=1, per_var=False, 
+        fix_beta_sum=False, fix_pi=False):
+        """Initialise Vireo model
+        Note, multiple initializations are highly recomended to avoid local 
+        optima.
+        
+        Parameters
+        ----------
+        n_obs : int 
+            Number of observations, i.e., samples
+        n_var : int
+            Number of variables, i.e., features or dimensions
+        n_components : int
+            Number of mixture components
+        fix_beta_sum: bool
+            Whether fix the concetration parameter of theta's posterior
+        fix_pi: bool
+            Whether fix the pi to the initial value
+        """
+        self.n_var = n_var
+        self.n_obs = n_obs
+        self.n_components = n_components
+        self.K = self.n_components # for short cut
+
+        self.per_var = per_var
+        self.fix_pi = fix_pi
+        self.fix_beta_sum = fix_beta_sum
+        
+        # initial values by random initial
+        self.set_init()
+        self.set_prior()
+        self.ELBO_ = np.zeros((0))
+
+    def set_init(self, beta_mu=None, beta_sum=None, pi_size=None, Z=None):
+        """Set initial values of the parameters to fit
+
+        Key properties
+        --------------
+        beta_mu: numpy.array (n_var, n_components)
+            Mean of Beta distribution as theta's posterior
+        beta_sum: numpy.array (n_var, n_components)
+            Concetration Beta distribution as theta's posterior
+        Z: numpy.array (n_obs, n_components)
+            Assignment probability of each observation to each cluster
+        pi_size: numpy.array (n_components)
+            Dirichlet parameters for posterior of cluster proportion
+        """
+        # initial key parameters
+        if beta_mu is not None:
+            self.beta_mu = beta_mu
+        else:
+            self.beta_mu = np.random.uniform(size=(self.n_var, self.K))
+
+        if beta_sum is not None:
+            self.beta_sum = beta_sum
+        else:
+            self.beta_sum = np.random.uniform(3, 10, size=(self.n_var, self.K))
+
+        if Z is not None:
+            self.Z_loglik = np.log(normalize(Z))
+        else:
+            self.Z_loglik = np.random.uniform(0.5, 1, size=(self.n_obs, self.K))
+
+        if pi_size is not None:
+            self.pi_size = pi_size
+        else:        
+            self.pi_size = np.ones((self.K))
+    
+    
+    def set_prior(self, beta_mu_prior=None, beta_sum_prior=None, 
+                  pi_prior=None, Z_prior=None):
+        """Set parameters of prior distribution for key variables:theta, pi, Z. 
+        The prior parameters are in the form of its according posterior
+        """
+        if beta_mu_prior is None:
+            beta_mu_prior = np.ones((self.n_var, self.K)) * 0.5
+        if beta_sum_prior is None:
+            beta_sum_prior = np.ones(beta_mu_prior.shape) * 2
+        self.theta_s1_prior = beta_mu_prior * beta_sum_prior
+        self.theta_s2_prior = (1 - beta_mu_prior) * beta_sum_prior
+
+        if Z_prior is not None:
+            self.Z_prior = Z_prior
+        else:
+            self.Z_prior = np.ones(self.Z.shape) / self.K
+
+        if pi_prior is not None:
+            self.pi_prior = pi_prior
+        else:
+            self.pi_prior = np.ones(self.K)
+
+    @property
+    def theta_s1(self):
+        """Beta concetration1 parameter for theta posterior"""
+        return self.beta_mu * self.beta_sum
+
+    @property
+    def theta_s2(self):
+        """Beta concetration2 parameter for theta posterior"""
+        return (1 - self.beta_mu) * self.beta_sum
+
+    @property
+    def pi(self):
+        return normalize(self.pi_size)
+    
+    @property
+    def Z(self):
+        return normalize_exp(self.Z_loglik)
+    
+    def update_theta_size(self, AD, DP):
+        """Coordinate ascent for updating theta posterior parameters
+        """
+        BD = DP - AD
+        _theta_s1 = self.theta_s1_prior + AD * self.Z
+        _theta_s2 = self.theta_s2_prior + BD * self.Z
+
+        self.beta_mu = _theta_s1 / (_theta_s1 + _theta_s2)
+        if self.fix_beta_sum == False:
+            self.beta_sum = _theta_s1 + _theta_s2
+        
+    def update_pi_size(self):
+        """Coordinate ascent for updating pi posterior parameters
+        """
+        self.pi_size = self.pi_prior + np.sum(self.Z, axis=0)
+
+    def update_Z_loglik(self, AD, DP):
+        """Coordinate ascent for updating assignment probability
+        """
+        BD = DP - AD
+        self.Z_loglik = (
+            np.log(self.Z_prior) + 
+            AD.T * digamma(self.theta_s1) + 
+            BD.T * digamma(self.theta_s2) - 
+            DP.T * digamma(self.theta_s1 + self.theta_s2)
+        )
+        
+    def get_ELBO(self, logbincoeff_=0):
+        """Calculating variational evidence lower bound with current parameters
+        """
+        _logLik_E = self.Z_loglik -  np.log(self.Z_prior)
+
+        LB_lik = np.sum(_logLik_E * self.Z)
+        KL_Z   = np.sum(entropy(self.Z, self.Z_prior, axis=-1))
+        KL_pi  = np.sum(dirichlet_entropy(self.pi_size, self.pi_prior, axis=-1))
+        KL_theta = np.sum(dirichlet_entropy(
+            np.stack((self.theta_s1, self.theta_s2), axis=2),
+            np.stack((self.theta_s1_prior, self.theta_s2_prior), axis=2)
+        ))
+
+        return logbincoeff_ + LB_lik - KL_Z - KL_pi - KL_theta
+    
+
+    def fit(self, AD, DP, max_iter=200, min_iter=5, epsilon_conv=1e-2,
+        verbose=True):
+        """Fit Vireo model with coordinate ascent
+        For high dimentional data or high number of components, please run 
+        with multiple initializations, e.g., 100 to avoid local optima, and
+        pick the one with highest ELBO.
+
+        Parameters
+        ----------
+        AD : scipy.sparse.csc_matrix (n_var, n_obs)
+            Sparse count matrix for alternative events or success
+        DP : scipy.sparse.csc_matrix (n_var, n_obs)
+            Sparse count matrix for depths or total trials
+        max_iter : int
+            Maximum number of iterations
+        min_iter :
+            Minimum number of iterations
+        epsilon_conv : float
+            Threshold for detecting convergence
+        verbose : bool
+            Whether print out log info
+        """
+        # check AD and DP's type: scipy.sparse or numpy.matrix
+        if isspmatrix(AD) == False or isspmatrix(DP) == False:
+            AD = np.asmatrix(AD)
+            DP = np.asmatrix(DP)
+
+        self.logbincoeff_ = np.sum(logbincoeff(
+            np.array(DP[DP>0]).reshape(-1), 
+            np.array(AD[DP>0]).reshape(-1)
+        ))
+        
+        ELBO = np.zeros(max_iter)
+        for it in range(max_iter):
+            self.update_Z_loglik(AD, DP)
+            self.update_theta_size(AD, DP)
+            if ~self.fix_pi: self.update_pi_size()
+
+            ELBO[it] = self.get_ELBO(self.logbincoeff_)
+
+            if it > min_iter:
+                if (ELBO[it] - ELBO[it - 1]) < -epsilon_conv:
+                    if verbose:
+                        print("Warning: Lower bound decreases! %.2f to %.2f!\n"
+                              %(ELBO[it - 1], ELBO[it]))
+                elif it == max_iter - 1:
+                    if verbose:
+                        print("Warning: VB did not converge!\n")
+                elif ELBO[it] - ELBO[it - 1] < epsilon_conv:
+                    break
+        
+        self.ELBO_ = np.append(self.ELBO_, ELBO[:it])
--- bbmix/models/statistics.py
+++ bbmix/models/statistics.py
@@ -6,6 +6,7 @@ Chen Qiao: cqiao@connect.hku.hk
 
 import numpy as np
 from scipy.stats import entropy
+from scipy.special import gammaln, digamma
 
 
 def bic_criterion(k, n, nll):
@@ -46,3 +47,87 @@ def icl_criterion(k, n, nll, probs):
         float: ICL score
     """
     return bic_criterion(k, n, nll) + entropy_criterion(probs)
+
+
+def dirichlet_entropy(X, X_prior=None, axis=-1):
+    """
+    Get the entropy for dirichlet distributions. If X_prior is not None, 
+    return the Kullback-Leibler divergence
+    See: https://docs.scipy.org/doc/scipy/reference/generated/scipy.stats.entropy.html
+    
+    Example
+    -------
+    theta_shapes1 = np.array([[0.3, 29.7], [3, 3], [29.7, 0.3]])
+    theta_shapes2 = np.array([[364, 24197], [5886, 7475], [6075, 397]])
+    dirichlet_entropy(theta_shapes2)
+    dirichlet_entropy(theta_shapes2, theta_shapes1)
+    """
+    _entropy = (
+        gammaln(X).sum(axis) -
+        gammaln(X.sum(axis)) -
+        ((X - 1) * digamma(X)).sum(axis) +
+        ((X.sum(axis) - X.shape[axis]) * digamma(X.sum(axis)))
+    )
+
+    if X_prior is not None:
+        _cross_entropy = (
+            gammaln(X_prior).sum(axis) -
+            gammaln(X_prior.sum(axis)) -
+            ((X_prior - 1) * digamma(X)).sum(axis) +
+            ((X_prior.sum(axis) - X_prior.shape[axis]) * digamma(X.sum(axis)))
+        )
+        _kl_divergence = _cross_entropy - _entropy
+
+        return _kl_divergence
+    else:
+        return _entropy
+
+
+def logfact(n):
+    """
+    Ramanujan's approximation of log n!
+    https://math.stackexchange.com/questions/138194/approximating-log-of-factorial
+    """
+    n = np.atleast_1d(n)
+    n[n == 0] = 1  # for computational convenience
+    return (
+        n * (np.log(n) - 1)
+        + np.log((n * (1. + 4. * (n * (1. + 2. * n))))) / 6.
+        + np.log(np.pi) / 2.
+    )
+
+
+def logbincoeff(n, k):
+    """
+    Ramanujan's approximation of log [n! / (k! (n-k)!)]
+    """
+    n_logfact = logfact(n)
+    k_logfact = logfact(k)
+    n_sub_k_logfact = logfact(n - k)
+    return n_logfact - k_logfact - n_sub_k_logfact
+
+
+def normalize(X, axis=-1):
+    """
+    Normalization of tensor with sum to 1.
+    
+    Example
+    -------
+    X = np.random.rand(3, 5, 8)
+    tensor_normalize(X, axis=1)
+    """
+    X_sum = np.sum(X, axis=axis, keepdims=True)
+    return X / X_sum
+
+
+def normalize_exp(X, axis=-1):
+    """
+    Amplify the log likelihood matrix by subtract the maximum.
+    
+    Example
+    -------
+    X = np.random.rand(3, 5, 8)
+    normalize_exp(X, axis=1)
+    """
+    X_max = np.max(X, axis=axis, keepdims=True)
+    return normalize(np.exp(X - X_max), axis=axis)
